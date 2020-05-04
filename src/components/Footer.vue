ac8eb8baf0d0d115915942b07194c119447f75a2
StanislavMyakishev <masiff-s@live.ru>
2020-05-04 19:00:11 +0300

    Added basic views with header/footer and styles dir

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    <p>Copyright 2020</p>
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/favorite">Favorite</router-link>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import "../styles/components/header";
+</style>
